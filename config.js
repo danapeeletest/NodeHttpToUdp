42b8efbff23e0a9f644bf7231f9103c8ce9758f1
Dana Peele <dana_peele@s5a.com>
2014-11-12 23:59:29 -0500

    Use metrics jar to send message to StatsD

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 var config = {};
 
-//The server and port to send the udp message to
-config.udp={};
-config.udp.servername = 'localhost';
-config.udp.port = '8125';
+//java settings
+config.java={};
+config.java.serverName='dev-of5';
+config.java.classpath='./metrics';
+config.java.configDir='/Users/921882/git/NodeHttpToUdp/metrics/config';
 
 //the port for the node server to listen on
 config.http={};
